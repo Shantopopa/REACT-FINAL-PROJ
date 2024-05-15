0c9d6ef6a77a686153db6bdffb719d1a2ba9a0b9
iyaqn <maryjulia.malagayo.cics@ust.edu.ph>
2024-05-16 04:27:11 +0800

    css

--- labact2/src/About.js
+++ labact2/src/About.js
@@ -1,55 +1,115 @@
 import React from "react";
 import "./App.css";
-
-function About () {
-    return(
-        <section className="section-white justify-content-center d-flex align-items-center">
-            <div className="container justify-content-center d-flex align-items-center">
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                        <h2 className="section-title">Meet the Team</h2>
-                        <p className="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-
-                    <div className="col-sm-6 col-md-4">
-                        <div className="team-item">
-                            <img src="https://paws.org.ph/wp-content/uploads/2023/06/DSC_0043-scaled-e1686895862885.jpg" className="team-img" alt="Team Picture" />
-                            <h3 className="name">NAME NAME</h3>
-                            <div className="team-info">
-                                <p className="name1">POSITION POSITION</p>
-                                <p className="name1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-sm-6 col-md-4">
-                        <div className="team-item">
-                            <img src="https://paws.org.ph/wp-content/uploads/2023/06/DSC_0043-scaled-e1686895862885.jpg" className="team-img" alt="Team Picture" />
-                            <h3 className="name">NAME NAME</h3>
-                            <div className="team-info">
-                                <p className="name1">POSITION POSITION</p>
-                                <p className="name1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-sm-6 col-md-4">
-                        <div className="team-item">
-                            <img src="https://paws.org.ph/wp-content/uploads/2023/06/DSC_0043-scaled-e1686895862885.jpg" className="team-img" alt="Team Picture" />
-                            <h3 className="name">NAME NAME</h3>
-                            <div className="team-info">
-                                <p className="name1">POSITION POSITION</p>
-                                <p className="name1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </section>
-    )
+function About() {
+return (
+<section className="section-white">
+<div className="container">
+<div className="row">
+<div className="col-md-12 text-center">
+<h2 className="section-title">Meet the Team</h2>
+<p className="section-subtitle">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+do
+eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut
+enim ad minim veniam, quis nostrud exercitation ullamco
+laboris
+nisi ut aliquip ex ea commodo consequat.
+</p>
+</div>
+<div className="col-sm-6 col-md-4">
+<div className="team-item">
+<img
+src="https://paws.org.ph/wp-content/uploads/2023/06/DSC_0043-scaled-e16868
+95862885.jpg"
+className="team-img"
+alt="Team Picture"
+/>
+<h3 className="name">NAME NAME</h3>
+<div className="team-info">
+<p className="name1">POSITION POSITION</p>
+<p className="name1">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed
+do eiusmod tempor incididunt ut labore et dolore magna
+aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation
+ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis
+aute
+irure dolor in reprehenderit in voluptate velit esse
+cillum
+dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia
+deserunt
+mollit anim id est laborum.
+</p>
+</div>
+</div>
+</div>
+<div className="col-sm-6 col-md-4">
+<div className="team-item">
+<img
+src="https://paws.org.ph/wp-content/uploads/2023/06/DSC_0043-scaled-e16868
+95862885.jpg"
+className="team-img"
+alt="Team Picture"
+/>
+<h3 className="name">NAME NAME</h3>
+<div className="team-info">
+<p className="name1">POSITION POSITION</p>
+<p className="name1">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed
+do eiusmod tempor incididunt ut labore et dolore magna
+aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation
+ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis
+aute
+irure dolor in reprehenderit in voluptate velit esse
+cillum
+dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia
+deserunt
+mollit anim id est laborum.
+</p>
+</div>
+</div>
+</div>
+<div className="col-sm-6 col-md-4">
+<div className="team-item">
+<img
+src="https://paws.org.ph/wp-content/uploads/2023/06/DSC_0043-scaled-e16868
+95862885.jpg"
+className="team-img"
+alt="Team Picture"
+/>
+<h3 className="name">NAME NAME</h3>
+<div className="team-info">
+<p className="name1">POSITION POSITION</p>
+<p className="name1">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed
+do eiusmod tempor incididunt ut labore et dolore magna
+aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation
+ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis
+aute
+irure dolor in reprehenderit in voluptate velit esse
+cillum
+dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia
+deserunt
+mollit anim id est laborum.
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+);
 }
-
 export default About;
